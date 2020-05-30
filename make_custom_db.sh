9eff816185935e7c2c3df43182c9e4b04b924533
Eirik Høye <eirikhoye@users.noreply.github.com>
2020-05-30 18:21:35 +0200

    Delete make_custom_db.sh

--- make_custom_db.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash/
-
-cd /home/jcdenton/
-python3 generate-mirtrace-rnatype-database.py --out-dir ../../custom_databases/hsa/ --species-abbrev hsa --species-verbosename homo_sapiens --mirna-seqs ../../mirna_reference/hsa-pre.fas
