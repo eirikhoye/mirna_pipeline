5ae3e75d54f5f5cc515ca37a2e6cf58c017663f8
Eirik Høye <eirikhoye@users.noreply.github.com>
2021-06-08 11:35:20 +0200

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,6 +3,6 @@ python scripts/create_config.py $1
 echo "run miRTrace"
 bash scripts/run_mirtrace.sh config
 echo "map to MirGeneDB with bowtie 1.2"
-bash cripts/bowtie_mirgenedb_human_fastafiles.sh
+bash scripts/bowtie_mirgenedb_human_fastafiles.sh
 echo "count miRNAs"
 Rscript scripts/featureCounts.R
